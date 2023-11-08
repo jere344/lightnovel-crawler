6bcec8052226a686201f54f0014f5b69f6a5b28f
unknown <jeremy.guerin34@yahoo.com>
2023-11-08 07:56:35 -0500

    remove forgotten check

--- lncrawl/bots/web2/flask_api/read_novel_info.py
+++ lncrawl/bots/web2/flask_api/read_novel_info.py
@@ -31,10 +31,7 @@ def get_novel_info(novel_folder: Path) -> Novel:
     # region Get Novel Stats
     novel_stats_file = Path(novel_folder / "stats.json")
 
-    if not novel_stats_file.exists():
-        shutil.copy(str(Path(__file__).parent / "_stats.json"), str(novel_stats_file))
-
-    # Retry 2 times. If it crash it is reset to default and retried.
+    # Retry twice. If it crash it is reset to default and retried.
     for _ in range(2):
         try :
             with open(novel_stats_file, "r", encoding="utf-8") as f:
@@ -46,6 +43,9 @@ def get_novel_info(novel_folder: Path) -> Novel:
 
         except Exception as e:
             print(f"Resetting novel stats for {novel_folder.name}: {e}")
+            if novel_stats_file.exists():
+                with open(novel_stats_file, "r", encoding="utf-8") as f:
+                    print(f.read())
             shutil.copyfile("./lncrawl/bots/web2/flask_api/_stats.json", novel_stats_file)
             continue
 
